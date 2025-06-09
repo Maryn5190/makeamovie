97f4d848b73421187ac5fbc2444ad5009d1ce38e
Maryn5190 <oluchi.nwankwo@gmail.com>
2025-06-10 00:47:33 +0100

    Create App.js

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div style={{ padding: '2rem', fontFamily: 'Arial' }}>
+      <h1>MakeaMovie</h1>
+      <p>Turn your words into magical videos.</p>
+    </div>
+  );
+}
+
+export default App;
